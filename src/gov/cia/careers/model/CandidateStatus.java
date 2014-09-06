3226dd431b3e0c208ee06c2cdf2bf58a844ea265
Lucas Churchill <lucas@agtlucas.com>
2014-09-06 18:18:38 -0300

    Update CandidateStatus Enum

--- src/gov/cia/careers/model/CandidateStatus.java
+++ src/gov/cia/careers/model/CandidateStatus.java
@@ -1,5 +1,17 @@
 package gov.cia.careers.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(name = "candidate-status")
 public enum CandidateStatus {
 
+	@XmlEnumValue("employed")
+	EMPLOYED,
+	
+	@XmlEnumValue("unemployed")
+	UNEMPLOYED;
+	
 }
