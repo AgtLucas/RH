da78bb6309e9242f5eb1dbe5992290f7aa443d53
Lucas Churchill <lucas@agtlucas.com>
2014-09-06 18:16:11 -0300

    Update CandidateEducation Enum

--- src/gov/cia/careers/model/CandidateEducation.java
+++ src/gov/cia/careers/model/CandidateEducation.java
@@ -1,5 +1,20 @@
 package gov.cia.careers.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(name = "education")
 public enum CandidateEducation {
+	
+	@XmlEnumValue("basic")
+	BASIC,
+	
+	@XmlEnumValue("secondary")
+	SECONDARY,
+	
+	@XmlEnumValue("graduation")
+	GRADUATION;
 
 }
