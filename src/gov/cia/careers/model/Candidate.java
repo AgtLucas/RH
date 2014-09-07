3ea6d352c1f9c3b81059de68253441e852bc1741
Lucas Churchill <lucas@agtlucas.com>
2014-09-07 15:10:56 -0300

    Imports...

--- src/gov/cia/careers/model/Candidate.java
+++ src/gov/cia/careers/model/Candidate.java
@@ -1,11 +1,9 @@
 package gov.cia.careers.model;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-
-import java.util.List;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Candidate {
