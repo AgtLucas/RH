ad88c9d63d23970234e15d05e4a9463c711773e5
Lucas Churchill <lucas@agtlucas.com>
2014-09-07 14:22:31 -0300

    Update Course

--- src/gov/cia/careers/model/Course.java
+++ src/gov/cia/careers/model/Course.java
@@ -2,4 +2,74 @@ package gov.cia.careers.model;
 
 public class Course {
 
+	private int id;
+	private String name;
+	private String location;
+	private String institution;
+	private CourseType type;
+	private CourseStatus status;
+
+	public Course() {
+		super();
+	}
+
+	public Course(int id, String name, String location, String institution,
+			CourseType type, CourseStatus status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.institution = institution;
+		this.type = type;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+
+	public CourseType getType() {
+		return type;
+	}
+
+	public void setType(CourseType type) {
+		this.type = type;
+	}
+
+	public CourseStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(CourseStatus status) {
+		this.status = status;
+	}
+
 }
