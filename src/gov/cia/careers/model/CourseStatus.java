0672fbbcc3c02edfac713781c284f2cdc4c0c0a4
Lucas Churchill <lucas@agtlucas.com>
2014-09-07 14:18:43 -0300

    /quit

--- src/gov/cia/careers/model/CourseStatus.java
+++ src/gov/cia/careers/model/CourseStatus.java
@@ -1,5 +1,17 @@
 package gov.cia.careers.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(name = "course-status")
 public enum CourseStatus {
+	
+	@XmlEnumValue("attending")
+	ATTENDING,
+	
+	@XmlEnumValue("quit")
+	QUIT;
 
 }
