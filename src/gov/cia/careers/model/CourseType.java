7645a84a12d3aec5628ad5cce422ae3209f0262d
Lucas Churchill <lucas@agtlucas.com>
2014-09-07 14:15:04 -0300

    Update CourseType

--- src/gov/cia/careers/model/CourseType.java
+++ src/gov/cia/careers/model/CourseType.java
@@ -1,5 +1,26 @@
 package gov.cia.careers.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(name = "course-type")
 public enum CourseType {
+	
+	@XmlEnumValue("vocational")
+	VOCATIONAL,
+	
+	@XmlEnumValue("technical")
+	TECHNICAL,
+	
+	@XmlEnumValue("superior")
+	SUPERIOR,
+	
+	@XmlEnumValue("idiom")
+	IDIOM,
+	
+	@XmlEnumValue("graduate")
+	GRADUATE;
 
 }
